8a218b026b39c133ce5d3a1039935656d7172dad
Anton Katkov <katkov.anton@gmail.com>
2012-05-24 12:57:55 +0400

    added example of untestalbe method and AppCode config files

---
+++ testability/Untestable Method/XPClassWithIUntestableMethod.h
@@ -0,0 +1,14 @@
+//
+// Created by vanger on 24.05.12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface XPClassWithIUntestableMethod : NSObject
+
+- (void)doSomethingWithObj1:(id)obj1 obj2:(id)obj2 fromDate:(NSDate *)fromDate toDate:(NSDate *)toDate forString1:(NSString *)string1 byString2:(NSString *)string2;
+@end
---
+++ testability/Untestable Method/XPClassWithIUntestableMethod.m
@@ -0,0 +1,21 @@
+//
+// Created by vanger on 24.05.12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "XPClassWithIUntestableMethod.h"
+
+
+@implementation XPClassWithIUntestableMethod {
+
+}
+- (void)doSomethingWithObj1:(id)obj1 obj2:(id)obj2 fromDate:(NSDate *)fromDate toDate:(NSDate *)toDate forString1:(NSString *)string1 byString2:(NSString *)string2 {
+	// ...
+	// some long and horrible processing of all those arguments!
+	// ...
+}
+
+
+@end
